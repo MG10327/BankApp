27c9f5148d91f2a9840cd6082be490d4b18d6dd6
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-09 20:33:44 -0500

    add basic setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Bank App</h1>
+
+    </>
+  )
+}
+
+export default App
