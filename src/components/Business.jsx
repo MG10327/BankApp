5d273f073b1ca4e367df2106ba6119b8a00285c5
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-09 22:43:28 -0500

    setup button basics

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -7,7 +7,12 @@ const Business = () => {
   return (
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2>You do the business, <br /> we'll handle the money.</h2>
+        <h2 className={`${styles.heading2}`}>You do the business, <br className='sm:block hidden'/> we'll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.</p>
+
+        <Button />
       </div>
 
 
