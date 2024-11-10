042afdc7eb31dfd9a03ef2192067a65a3de96b5c
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-09 22:00:30 -0500

    add hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,6 @@ const Hero = () => {
               <span className='text-gradient'>
                 Generation.
               </span>{" "}
-              Payment Method.
             </h1>
             <div className="ss:flex hidden md:mr-4 mr-0">
               <GetStarted />
@@ -30,7 +29,12 @@ const Hero = () => {
           </div>
         </div>
 
-        <h1></h1>
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>Payment Method.</h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
 
       </div>
     </section>
