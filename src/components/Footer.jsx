d14dd64ec6d3060c836724aa9f5dd81cb4211a72
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-10 00:37:16 -0500

    base footer setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { footerLinks } from '../constants'
+import { footerLinks, socialMedia } from '../constants'
 import styles from '../Style'
 import { logo } from '../assets'
 
@@ -18,12 +18,30 @@ const Footer = () =>  (
                 {footerLink.title}
               </h4>
 
-              <ul>
+              <ul className='list-none mt-4'>
                 {footerLink.links.map((link, index) =>
-                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}></li>
+                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length -1 ? "mb-4" : "mb-0"}`}></li>
                 )}
               </ul>
             </div>
+          )}
+        </div>
+      </div>
+
+
+
+      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
+        <p className='font-poppins font-medium text-center text-[18px] leading-[27px] text-white'>2024 Hoobank. All Rights Reserved.</p>
+
+        <div className='flex flex-row md:mt-0 mt-6'>
+          {socialMedia.map((social,index) =>
+            <img
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : "mr-0"}`}
+
+            />
           )}
         </div>
       </div>
