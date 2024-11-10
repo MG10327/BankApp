347db9ac2721f5adbaca7d263c7437c87cd6cbae
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-09 22:37:02 -0500

    imports

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles, {layout} from '../Style'
+import Button from './Button'
+import { features } from '../constants'
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2>You do the business, <br /> we'll handle the money.</h2>
+      </div>
+
+
+    </section>
   )
 }
 
