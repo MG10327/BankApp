2516a94e683b60a2dfcad8c90302b4ec7e983eea
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-10 00:30:25 -0500

    begin footer setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { footerLinks } from '../constants'
+import styles from '../Style'
+import { logo } from '../assets'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
+const Footer = () =>  (
+    <section className={`${styles.flexCenter} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div>
+          <img src={logo} alt="hoobank" className='w-[266px] h-[72px] object-contain' />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
+        </div>
+
+        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+          {footerLinks.map((footerLink) =>
+            <div key={footerLink.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
+              <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
+                {footerLink.title}
+              </h4>
+
+              <ul>
+                {footerLink.links.map((link, index) =>
+                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}></li>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
   )
-}
 
 export default Footer
