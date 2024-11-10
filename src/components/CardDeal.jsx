fb51d63a721249b2280c1be19e8d028ba975851c
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-09 23:35:14 -0500

    card deal text

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, {layout} from "../Style"
+import Button from './Button'
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in a few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}></p>
+      </div>
+    </section>
   )
 }
 
